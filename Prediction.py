691fd998a29737eb734d8ef457a0737f8f3dfd59
Ovi <botsch_ovidiu27@yahoo.com>
2021-05-30 11:26:11 +0300

    Adding comments

--- Prediction.py
+++ Prediction.py
@@ -12,27 +12,35 @@ def app():
     import pandas as pd
     from PIL import Image
 
+    #this class is used for inserating in Firebase data
     class Forecast_data_DB:
         def __init__(self, cases, data):
             self.cases = cases
             self.data = data
 
+    #initializing database
+    firebase = firebase.FirebaseApplication("https://covid-19-535b8-default-rtdb.firebaseio.com/", None)
+
     st.title('Forecasting App')
     st.write('Welcome to Covid-19 Forecasting App! With this app you will be able to see how Covid-19 cases may increase or decrease in the future.')
-    # LOADING DATA IN FIREBASE
-    firebase = firebase.FirebaseApplication("https://covid-19-535b8-default-rtdb.firebaseio.com/", None)
+
     st.sidebar.title("Forecasting parameters")
 
+    #printing a Covid-19 image in forecasting page
     image = Image.open('D:\Python_implements\Proiect_IPDP\covid-cells.jpg')
     st.image(image)
 
+    #List of countries we select for prediction or statistics data
     countries_list = ["Germany", "Italy", "Romania", "Spain", "Greece", "Switzerland", "Hungary", "Belarus", "Austria",
                       "Croatia", "Slovenia"]
+
+    #this i variable (which takes random values) was required because we need different name for each app calling
     i = random.randint(0, 100000000)
     selected_country = st.sidebar.selectbox("Select country", countries_list)
     cred_obj = firebase_admin.credentials.Certificate('covid-19-535b8-firebase-adminsdk-75wgs-65e6050775.json')
     default_app = firebase_admin.initialize_app(cred_obj, {'databaseURL': 'https://covid-19-535b8-default-rtdb.firebaseio.com/'}, str(selected_country) + str(i))
 
+    #This code gives us the last date which was inserated in database, in /covid-19-535b8-default-rtdb field
     node = ""
     node += '/covid-19-535b8-default-rtdb/'
     node += selected_country
@@ -61,14 +69,19 @@ def app():
 
     for country in countries_list:
         if country == selected_country:
+            #take data(active cases and date) from selected country API
             country_api = "https://api.covid19api.com/country/"
-            db_field = '/covid-19-535b8-default-rtdb/'
             country_api += country
             country_api += "/status/confirmed/live"
             response = requests.get(country_api)
+
+            #field in DB in wich we write data
+            db_field = '/covid-19-535b8-default-rtdb/'
             db_field += country
 
+            #take data from API starting with row 100
             for i in range(100, len(response.json())):
+                #taking date for each row in API
                 counter = 0
                 year_api = ""
                 month_api = ""
@@ -88,6 +101,7 @@ def app():
                     else:
                         break
 
+                #Write new data in API only if API date for each dataset row is greater than last date inserted in Database
                 if (int(year_api) > int(year_db)) or (int(year_api) == int(year_db) and int(month_api) > int(month_db)) or (int(year_api) == int(year_db) and int(month_api) == int(month_db) and int(day_api) > int(day_db)):
                     obj = Forecast_data_DB(str(int(response.json()[i]["Cases"]) - int(response.json()[i - 1]["Cases"])), response.json()[i]["Date"])
                     if int(obj.cases) != 0:
@@ -101,26 +115,22 @@ def app():
                         print(day_api)
                         result = firebase.post(db_field, json)
                         print(json["date"])
-
-    # STREAMLIT APP
-    START = "2021-01-22"
+            break
 
     db_name = "/covid-19-535b8-default-rtdb/"
     db_name += selected_country
     print("db_name " + db_name)
     data_country = db.reference(db_name, default_app).get()
-    for key, value in reversed(list(ref.items())):
-        FINAL = value["date"]
-        break
 
-    days = []
-    for i in range(20, 60):
-        days.append(i)
+    #Number of days for prediction
+    days = [7, 14, 30]
     n_days = st.sidebar.selectbox("Select number of days for prediction:", days)
     period = n_days
 
 
     @st.cache
+    #this function takes data from database and puts in a dataframe number of cases and date
+    #if number of cases is 0, value is replaced with nan
     def load_data(data_country):
         data = {'cases': [],
                 'date': []}
@@ -146,12 +156,12 @@ def app():
     data = load_data(data_country)
     data_load_state.text("Loading data...done!")
     print(data)
-    # data.fillna(method='ffill')
 
     st.subheader('Raw data')
     st.write(data.tail())
 
-
+    #function for data processing before forecasting and plotting data
+    #if it is found nan data, it is replaced with 0
     def plot_raw_data():
         fig = go.Figure()
         newdata = []
@@ -174,29 +184,34 @@ def app():
 
     plot_raw_data()
 
-    # Forecasting
+    # Forecasting part
+
+    #Training data
     df_train = data[['date', 'cases']]
     df_train = df_train.rename(columns={"date": "ds", "cases": "y"})
 
-    # df_train['y'] = df_train['y'] + 1
-    # df_train['y'] = np.log(df_train['y'])
+    #Prediction
     m = Prophet(weekly_seasonality=True, yearly_seasonality=True)
     m.fit(df_train)
     future = m.make_future_dataframe(periods=period)
     forecast = m.predict(future)
 
-    # forecast['yhat'] = np.exp(forecast['yhat'])-1
+    #Because it happens to have descending trend more time, we could have negative values resulted from prediction
+    #As a precautionary measure, replace negative predicted data with 0
     for x in range(len(forecast['yhat'])):
         if forecast['yhat'][x] <= 0:
             forecast['yhat'][x] = 0
-    print(forecast)
+
+    #Print last predicted data
     st.subheader('Forecast data')
     st.write(forecast.tail())
 
+    #Plot forecast data
     st.write('forecast data')
     fig1 = plot_plotly(m, forecast)
     st.plotly_chart(fig1)
 
+    #Plot some components such as trend, weekly, yearly etc
     st.write('forecast components')
     fig2 = m.plot_components(forecast)
     st.write(fig2)
